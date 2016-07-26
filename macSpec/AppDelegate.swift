d5bd85506d9103d49596bf61bc3725eaf55f118a
Milko Daskalov <mdaskalov@outlook.com>
2016-07-26 16:15:26 +0200

    Initial Commit

---
+++ macSpec/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macSpec
+//
+//  Created by Milko Daskalov on 26.07.16.
+//  Copyright © 2016 Milko Daskalov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
