273fe8dc053dbcdc2a931393338e81c4a22e78fc
Milko Daskalov <mdaskalov@outlook.com>
2025-01-02 19:56:04 +0100

    updated app file

---
+++ macSpec/macSpecApp.swift
@@ -0,0 +1,18 @@
+//
+//  macSpecApp.swift
+//  macSpec
+//
+//  Created by Milko Daskalov on 21.12.24.
+//  Copyright © 2024 Milko Daskalov. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct macSpecSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
