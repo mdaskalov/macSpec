3d7dac7a6d855fba9a8af053433b58212b0ef283
Milko Daskalov <mdaskalov@outlook.com>
2025-01-02 19:55:43 +0100

    added app view

---
+++ macSpec/AppView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  macSpecSwiftUI
+//
+//  Created by Milko Daskalov on 21.12.24.
+//
+
+import SwiftUI
+
+
+struct AppView: View {
+    @StateObject var data = SpecData()
+    @State var delay = 0.0
+    @State var peaks: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                WaveView(data: data)
+                    .aspectRatio(1.6, contentMode: .fit)
+                    .frame(maxHeight: 150)
+                VStack(alignment: .leading) {
+                    Picker("", selection: $data.source) {
+                        Text("Audio").tag(Source.audio)
+                        Text("Test").tag(Source.test)
+                        Text("Generated").tag(Source.generated)
+                    }
+                    .pickerStyle(.segmented)
+                    .labelsHidden()
+                    .fixedSize()
+                    Slider(value: $data.testPhase, in: 0...data.maxTestPhase)
+                        .disabled(data.source == .audio)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            SpecView(data: data)
+            Spacer()
+        }
+        .padding()
+        .frame(minWidth: 600, minHeight: 400)
+        .onAppear() {
+            data.startSampling()
+        }
+    }
+}
+
+#Preview {
+    AppView()
+}
