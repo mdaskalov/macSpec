77f2b6c21af5be2f98c58c3e93f9251637a866ef
Milko Daskalov <mdaskalov@outlook.com>
2026-07-21 17:40:32 +0200

    use caculated frame data

--- macSpec/AppView.swift
+++ macSpec/AppView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 
 struct AppView: View {
     @StateObject var data = SpecData()
-    @State var delay = 0.0
-    @State var peaks: CGFloat = 0
 
     private var sourceBinding: Binding<Source> {
         Binding(
@@ -28,28 +26,51 @@ struct AppView: View {
     var body: some View {
         VStack {
             HStack {
-                WaveView(data: data)
+                WaveView(frame: data.frame)
                     .aspectRatio(1.6, contentMode: .fit)
                     .frame(maxHeight: 150)
                 VStack(alignment: .leading) {
-                    Picker("", selection: sourceBinding) {
-                        Text("Audio").tag(Source.audio)
-                        Text("Test").tag(Source.test)
-                        Text("Generated").tag(Source.generated)
+                    HStack {
+                        Picker("", selection: sourceBinding) {
+                            Text("Audio").tag(Source.audio)
+                            Text("Generated").tag(Source.generated)
+                            Text("Test").tag(Source.test)
+                        }
+                        .pickerStyle(.segmented)
+                        .labelsHidden()
+                        .fixedSize()
+                        HStack {
+                            Slider(value: $data.testPhase, in: 0...data.maxTestPhase)
+                                .disabled(data.source == .audio)
+                            Text("\(data.testFrequency ?? 0, format: .number.precision(.fractionLength(0))) Hz")
+                                .lineLimit(1)
+                                .monospacedDigit()
+                        }
+                        .opacity(data.testFrequency == nil ? 0 : 1)
+                        .disabled(data.testFrequency == nil)
                     }
-                    .pickerStyle(.segmented)
-                    .labelsHidden()
-                    .fixedSize()
-                    Slider(value: $data.testPhase, in: 0...data.maxTestPhase)
-                        .disabled(data.source == .audio)
+                    HStack {
+                        Text("Floor")
+                        Slider(value: $data.dbFloor, in: -90...(0))
+                    }
+                    HStack {
+                        Text("Bar Decay")
+                        Slider(value: $data.barDelay, in: 0...0.3)
+                    }
+                    HStack {
+                        Text("Peak Hold")
+                        Slider(value: $data.peakDelay, in: 0...200)
+                    }
+                    Text(String(format: "floor: %.0f, barDecay: %.3f peakDelay: %d, SampleRate: %.1f kHz, %.1f Hz",
+                                data.dbFloor, data.barDelay, data.peakDelay, (data.sampleRate ?? 0) / 1000, data.testFrequency ?? 0 ))
                 }
                 .frame(maxWidth: .infinity)
             }
-            SpecView(data: data)
+            SpecView(frame: data.frame)
             Spacer()
         }
         .padding()
-        .frame(minWidth: 600, minHeight: 400)
+        .frame(minWidth: 650, minHeight: 400)
         .task {
             data.startSampling()
         }
